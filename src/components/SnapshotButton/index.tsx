302c40e48dd00e14e3bb09670e57b0b20f922bdd
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2026-05-11 17:20:44 +0900

    테스트 코드 ci

--- src/components/SnapshotButton/index.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/SnapshotButton/index.tsx
-
-export default function SnapshotButton({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <button className="bg-yellow-500">{children}</button>;
-}
